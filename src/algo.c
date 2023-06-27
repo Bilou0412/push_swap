a2df385f31a19746838ed278a28fd415744c0b1d
Bilel MOUDACHE <bmoudach@paul-f4Br1s2.clusters.42paris.fr>
2023-06-27 20:57:40 +0200

    ok

--- src/algo.c
+++ src/algo.c
@@ -6,7 +6,7 @@
 /*   By: bmoudach <bmoudach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 17:49:13 by bmoudach          #+#    #+#             */
-/*   Updated: 2023/06/27 05:39:09 by bmoudach         ###   ########.fr       */
+/*   Updated: 2023/06/27 20:52:49 by bmoudach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,66 +29,34 @@ int	is_sort(t_stake **pile)
 	return (1);
 }
 
-int	index_to_moov(t_stake **pile, int bits_rank)
-{
-	t_stake	*tmp;
-	int		i;
-
-	tmp = *pile;
-	i = 0;
-	while (tmp)
-	{
-		if (!((tmp->index >> bits_rank) & 1))
-			return (i);
-		tmp = tmp->next;
-		i++;
-	}
-	return (-1);
-}
-
 void	sort_pile(t_stake **pile_a, t_stake **pile_b, int bit_max)
 {
 	int bit_rank;
-	int to_move;
 	t_stake *tmp;
+	int	size;
 
 	bit_rank = 0;
-	to_move = 0;
-	tmp = *pile_a;
 	while (bit_rank < bit_max)
 	{
-		if (is_sort(pile_a) && ft_lstsize_ps(pile_b) == 0)
-			return ;
-		while (to_move > -1)
+		size = ft_lstsize_ps(pile_a);
+		while (size)
 		{
-			to_move = index_to_moov(pile_a, bit_rank);
-			if (to_move == 0)
+			tmp = *pile_a;
+			if (((tmp->index >> bit_rank) & 1) == 0)
 			{
 				pb(pile_a, pile_b);
-				to_move = index_to_moov(pile_a, bit_rank);
+				size--;
 			}
-			else if (to_move > ft_lstsize_ps(pile_a) / 2)
+			else
 			{
-				while (ft_lstsize_ps(pile_a) - to_move)
-				{
-					rra(pile_a);
-					to_move = index_to_moov(pile_a, bit_rank);
-				}
-			}
-			else if (to_move <= ft_lstsize_ps(pile_a) / 2 && to_move > -1)
-			{
-				while (to_move)
-				{
-					ra(pile_a);
-					to_move = index_to_moov(pile_a, bit_rank);
-				}
+				ra(pile_a);
+				size--;
 			}
 		}
-		bit_rank++;
-		to_move = index_to_moov(pile_a, bit_rank);
-		while (ft_lstsize_ps(pile_b))
+		while (ft_lstsize_ps(pile_b) > 0)
 		{
 			pa(pile_b, pile_a);
 		}
+		bit_rank++;
 	}
 }
